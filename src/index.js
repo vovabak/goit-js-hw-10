7e2471c02ebf55503482f16264e148a75ce7ebce
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-27 11:15:38 +0300

    adds murkup

--- src/index.js
+++ src/index.js
@@ -14,13 +14,44 @@ refs.searchBox.addEventListener('input', onSearchBox);
 function onSearchBox(e) {
 
     
-    const querry = e.currentTarget.value;
+    const querry = e.currentTarget.value.trim();
 
+    if (querry === '') {
+        refs.countryList.innerHTML = '';
+        return
+    };
+    
     
     fetch(`${BASE_URL}${querry}`)
         .then(result => result.json())
-        .then(countries => refs.countryList.innerHTML = countries.map(countrie => `<img src=${countrie.flags.svg} width=30px height=15px alt=${countrie.name.official}></img>`))
-        // .then(countries => countries.map(countrie =>`<li><img src=${countrie.flags.svg} alt=${countrie.name.official}><p>${countrie.name.official}</p><p>${countrie.population}</p><p>${...Object.values(countrie.languages)}</p><p>${...countrie.capital}</p></li>`))
+        .then(countries => {
+            if (countries.length > 10) {
+                // window.alert("Too many matches found. Please enter a more specific name.");
+                return
+            }
+            else if (countries.length === 1) {
+                console.log(countries);
+                
+                refs.countryList.innerHTML = `<li>
+                                <img src=${countries[0].flags.svg} width=30px height=15px alt=${countries[0].name.official}/>
+                                <h4>${countries[0].name.official}</h4>                                
+                            </li>`
+                refs.countryInfo.innerHTML = `<p>capital: ${countries[0].capital}</p>
+                                <p>population: ${(countries[0].population / 1000000).toFixed(2)} mln</p>
+                                <p>official languages: ${Object.values(countries[0].languages)}</p>`
+            }
+            else
+            {
+                refs.countryInfo.innerHTML = '';
+                
+                refs.countryList.innerHTML = countries.map(countrie => {
+                    return `<li>
+                                <img src=${countrie.flags.svg} width=30px height=15px alt=${countrie.name.official}/>
+                                <h4>${countrie.name.official}</h4>                                
+                            </li>`
+                }).join('')
+            }
+        })        
 }
 
 // function fetchCountrie() {
